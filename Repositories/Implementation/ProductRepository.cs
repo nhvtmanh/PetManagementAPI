ec9f9843623eaf538fb7da64365ee486acccddd3
nhvtmanh <nhvtmanh@gmail.com>
2025-02-07 21:42:22 +0700

    fix

--- Repositories/Implementation/ProductRepository.cs
+++ Repositories/Implementation/ProductRepository.cs
@@ -14,6 +14,38 @@ namespace PetManagementAPI.Repositories.Implementation
             _dbContext = dbContext;
         }
 
+        public async Task<FavoriteProduct?> AddFavorite(string customerId, Guid productId)
+        {
+            if (!_dbContext.FavoriteProducts.Any(x => x.CustomerId == customerId && x.ProductId == productId))
+            {
+                var favoriteProduct = new FavoriteProduct
+                {
+                    CustomerId = customerId,
+                    ProductId = productId
+                };
+
+                _dbContext.FavoriteProducts.Add(favoriteProduct);
+                await _dbContext.SaveChangesAsync();
+
+                return favoriteProduct;
+            }
+            return null;
+        }
+
+        public async Task<FavoriteProduct?> DeleteFavorite(string customerId, Guid productId)
+        {
+            var favoriteProduct = await _dbContext.FavoriteProducts
+                .FirstOrDefaultAsync(x => x.CustomerId == customerId && x.ProductId == productId);
+
+            if (favoriteProduct != null)
+            {
+                _dbContext.FavoriteProducts.Remove(favoriteProduct);
+                await _dbContext.SaveChangesAsync();
+                return favoriteProduct;
+            }
+            return null;
+        }
+
         public async Task<IEnumerable<Product>> GetByCategory(string name)
         {
             return await _dbContext.Products
@@ -25,6 +57,14 @@ namespace PetManagementAPI.Repositories.Implementation
         {
             return await _dbContext.Products
                 .Where(p => p.Name.ToLower().Contains(name.ToLower()))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<FavoriteProduct>> GetFavorite(string customerId)
+        {
+            return await _dbContext.FavoriteProducts
+                .Include(x => x.Product)
+                .Where(x => x.CustomerId == customerId)
                 .ToListAsync();
         }
     }
